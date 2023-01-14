59ce29288052d5a3a8f7d10f010faa8c934cde1a
Luis Vicente Albarracin <albarracinluisvicente@gmail.com>
2023-01-14 17:28:29 -0500

    cambios en los certificados

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,301 +2,34 @@ const certificados = document.getElementById("certificados");
 const about_me = document.getElementById("aboutme");
 const proyectos = document.getElementById("proyectos");
 
-
-certificados.addEventListener("click", () => {
+function add_certificados(certificados){
   const main = document.getElementById("main");
-  main.innerHTML = `
-  <section class="container">
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/platzi/ingenieria-software.jpg"
-            class="card-img-top"
-            alt="Certificado de Ingeniería de Software"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Ingeniería de Software</h5>
-            <p class="card-text">
-              Certificado de Introducción a la Ingeniería de Software -
-              Apps.co y Platzi.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/platzi/git-github.jpg"
-            class="card-img-top"
-            alt="Certificado de Git y Github"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Git Y Github</h5>
-            <p class="card-text">
-              Certificado de Git y Github - Apps.co y Platzi.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="./assets/certificados-sololearn/java.png"
-            class="card-img-top"
-            alt="certificación en Java"
-          />
-          <div class="card-body">
-            <h5 class="card-title">JAVA</h5>
-            <p class="card-text">Curso de java en sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sololearn/sql.png"
-            class="card-img-top"
-            alt="Certificado de SQL"
-          />
-          <div class="card-body">
-            <h5 class="card-title">SQL</h5>
-            <p class="card-text">Curso de SQL en Sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sololearn/html.jpg"
-            class="card-img-top"
-            alt="Certificado de HTML"
-          />
-          <div class="card-body">
-            <h5 class="card-title">HTML</h5>
-            <p class="card-text">Curso de HTML en Sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sololearn/javascript.png"
-            class="card-img-top"
-            alt="Certificado de Javascript"
-          />
-          <div class="card-body">
-            <h5 class="card-title">JAVASCRIPT</h5>
-            <p class="card-text">Curso de Javascript en Sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sololearn/python.png"
-            class="card-img-top"
-            alt="Certificado de Python"
-          />
-          <div class="card-body">
-            <h5 class="card-title">CSS</h5>
-            <p class="card-text">Curso de CSS en Sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sololearn/php.png"
-            class="card-img-top"
-            alt="Certificado de PHP"
-          />
-          <div class="card-body">
-            <h5 class="card-title">PHP</h5>
-            <p class="card-text">Curso de PHP en Sololearn.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/acta de grado.jpg"
-            class="card-img-top"
-            alt="Certificado de acta de grado"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Acta de Grado Bachillerato</h5>
-            <p class="card-text">
-              Acta individual de grado undécimo grado.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/diploma.jpg"
-            class="card-img-top"
-            alt="Certificado de acta de grado"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Acta de Diplima de Bachillerato</h5>
-            <p class="card-text">
-              Acta individual de grado undécimo grado.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/habilidades-web.jpg"
-            class="card-img-top"
-            alt="Certificado en aplicaciones web"
-          />
-          <div class="card-body">
-            <h5 class="card-title">
-              Habilidades en programación con énfasis en aplicaciones web de
-              Misión TIC 2022
-            </h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/técnico-laboral.jpg"
-            class="card-img-top"
-            alt="Certificado de mision tic"
-          />
-          <div class="card-body">
-            <h5 class="card-title">MISION TIC</h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/Certificado_Mision_c1.jpg"
-            class="card-img-top"
-            alt="Certificado de mision tic"
-          />
-          <div class="card-body">
-            <h5 class="card-title">MISION TIC</h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/Certificado_Mision_c2.jpg"
-            class="card-img-top"
-            alt="Certificado de mision tic"
-          />
-          <div class="card-body">
-            <h5 class="card-title">MISION TIC</h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/Certificacion_Ciclo_3_.jpg"
-            class="card-img-top"
-            alt="Certificado de mision tic"
-          />
-          <div class="card-body">
-            <h5 class="card-title">MISION TIC</h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-misiontic/Certificacion_Ciclo_4.jpg"
-            class="card-img-top"
-            alt="Certificado de mision tic"
-          />
-          <div class="card-body">
-            <h5 class="card-title">MISION TIC</h5>
-            <p class="card-text">Mision TIC.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sena/base-datos.jpg"
-            class="card-img-top"
-            alt="Certificado de curso de base de datos y sistemas de gestión"
-          />
-          <div class="card-body">
-            <h5 class="card-title">SENA</h5>
-            <p class="card-text">
-              Base de datos generalidades y sistemas de gestión.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sena/calidad-software.jpg"
-            class="card-img-top"
-            alt="Certificado de curso de aplicación de la calidad del software en el proceso de desarrollo"
-          />
-          <div class="card-body">
-            <h5 class="card-title">SENA</h5>
-            <p class="card-text">
-              Aplicación de la calidad del software en el proceso de
-              desarrollo.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sena/habilidades-digitales.jpg"
-            class="card-img-top"
-            alt="Certificado de curso de desarrollo de habilidades digitales para la gestión de la información"
-          />
-          <div class="card-body">
-            <h5 class="card-title">SENA</h5>
-            <p class="card-text">
-              Desarrollo de habilidades digitales para la gestión de la
-              información.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img
-            src="assets/certificados-sena/soporte.jpg"
-            class="card-img-top"
-            alt="Certificado de curso de Procesos de soporte tecnico para el mantenimiento de equipos de computo"
-          />
-          <div class="card-body">
-            <h5 class="card-title">SENA</h5>
-            <p class="card-text">
-              Procesos de soporte tecnico para el mantenimiento de equipos
-              de computo.
-            </p>
-          </div>
-        </div>
+  main.innerHTML = "";
+  for (let i = 0; i < certificados.length; i++){
+    main.innerHTML += `
+    <div class="card col-12 col-sm-6 col-md-4">
+      <img class="card-img-top" src="${certificados[i].imagen}" alt="${certificados[i].nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${certificados[i].nombre}</h5>
+        <p class="card-text">${certificados[i].empresa}</p>
+        <a href="${certificados[i].link}" class="btn btn-primary">Ver certificado</a>
       </div>
     </div>
-  </section>
-
   `;
+  }
+  
+}
 
+certificados.addEventListener("click", () => {
+  const requestURL = 'assets/json/certificados.json';
+  fetch(requestURL)
+    .then((response) => response.json())
+    .then((data) => {
+      add_certificados(data);
+    })
+    .catch((err) => console.log('err'))
 });
+
 
 about_me.addEventListener("click", () => {
   const main = document.getElementById("main");
